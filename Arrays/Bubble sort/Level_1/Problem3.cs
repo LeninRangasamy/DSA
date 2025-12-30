88d0e4861a715f584edac90b8dc1a345427a49c6
Lenin <leninlenin077@gmail.com>
2025-12-30 20:18:01 +0530

    Solution for problem 3

---
+++ Arrays/Bubble sort/Level_1/Problem3.cs
@@ -0,0 +1,32 @@
+using System;
+class BubbleSort{
+  public static void Main(string[] args)
+  {
+    int[] arr={4,1,5,2,3};
+    int size =arr.Length-1;
+    //Outer loop for number of passes
+    for(int i=0;i<size;i++){
+      bool swapped=false;  //This value is re-setted when loop runs that next iteration
+      for(int j=0;j<size-i;j++){
+        if(arr[j]>arr[j+1]){
+          //Swap elements when 'if' condition true'
+          int temp=arr[j];
+          arr[j]=arr[j+1];
+          arr[j+1]=temp;
+          swapped=true; //Updating the variable to break initial sorts
+        }
+      }
+      Console.WriteLine($"\nPass {i}");
+      foreach(var a in arr) Console.Write(a+",");
+  
+      if(!swapped){
+        Console.WriteLine($"\nNo swaps in this pass. Array sorted");
+        break;
+      }
+      
+    }
+    
+    Console.WriteLine("Sorted array");
+    foreach(var a in arr) Console.Write(a+",");
+  }
+}
